e07ccb7bc8f3b9d867328b6b5d4f5733001f96b7
Omar Faruqe <faruqe.cse@gmail.com>
2016-03-31 14:08:53 -0400

    Initial Commit

---
+++ PingPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by Omar Faruqe on 2016-03-31.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PingPong/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  PingPong
+//
+
+//  Copyright (c) 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
